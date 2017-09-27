91e5d507c9d4e7bb64e60ab36123b45c641e0c95
Mahendra Tennakoon <mahendra.tennakoon@pearson.com>
2017-09-27 21:13:00 +0530

    Initial commit

---
+++ src/main/java/controllers/Controller.java
@@ -0,0 +1,13 @@
+package controllers;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+
+public class Controller {
+    @FXML
+    private Button btn_test;
+
+    public void test() {
+        System.out.println("Hello World!");
+    }
+}
