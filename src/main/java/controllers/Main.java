d64d67a7e0fbc54c1aa94d7e61d3d9ea632afdd0
Mahendra Tennakoon <mahendrathennakoon@gmail.com>
2017-10-30 23:31:09 +0530

    Install and configure logback

--- src/main/java/controllers/Main.java
+++ src/main/java/controllers/Main.java
@@ -6,13 +6,18 @@ import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
 import javafx.stage.StageStyle;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 public class Main extends Application {
+    static final Logger LOG = LoggerFactory.getLogger(Main.class);
+
 
     private Stage stage;
 
     @Override
     public void start(Stage primaryStage) throws Exception{
+        LOG.info("Starting application.");
         stage = primaryStage;
         Parent root = FXMLLoader.load(getClass().getClassLoader().getResource("views/login.fxml"));
         Scene loginScene = new Scene(root);
