e45500d85a63a139cf361ae50a661362fb8f2974
Mahendra Tennakoon <mahendrathennakoon@gmail.com>
2017-11-03 13:36:16 +0530

    Supplier management functionality.

--- src/main/java/controllers/HomeController.java
+++ src/main/java/controllers/HomeController.java
@@ -1,5 +1,62 @@
 package controllers;
 
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.layout.BorderPane;
+import javafx.scene.layout.StackPane;
+
 public class HomeController {
-    
+
+    @FXML
+    private Button sidebar_business_overview;
+    @FXML
+    private Button sidebar_human_resources;
+    @FXML
+    private Button sidebar_sales;
+    @FXML
+    private Button sidebar_inventory;
+    @FXML
+    private Button sidebar_suppliers;
+    @FXML
+    private Button sidebar_finance;
+    @FXML
+    private Button sidebar_customers;
+    @FXML
+    private Button sidebar_deliveries;
+    @FXML
+    private Button sidebar_advertising;
+    @FXML
+    private Button sidebar_infrastructure;
+    @FXML
+    private Button sidebar_useraccounts;
+    @FXML
+    private BorderPane border_pane_home;
+    @FXML
+    private StackPane stackpane_home_center;
+    @FXML
+    private Label lbl_logged_in_user;
+
+    /**
+     * load Supplier FXML into center of the home borderpane.
+     */
+    public void loadSupplierPanel() {
+        loadCenterPanel("/views/Supplier.fxml");
+    }
+
+    /**
+     * Loads the FXML scene to the center of home borderpane.
+     *
+     * @param fxml_path
+     */
+    public void loadCenterPanel(String fxml_path) {
+        stackpane_home_center.getChildren().clear();
+        try {
+            stackpane_home_center.getChildren().clear();
+            stackpane_home_center.getChildren().add(FXMLLoader.load(getClass().getResource(fxml_path)));
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
 }
---
+++ src/main/java/dao/SupplierDAO.java
@@ -0,0 +1,38 @@
+package dao;
+
+import models.SupplierEntity;
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.boot.MetadataSources;
+import org.hibernate.boot.registry.StandardServiceRegistry;
+import org.hibernate.boot.registry.StandardServiceRegistryBuilder;
+
+public class SupplierDAO {
+    protected SessionFactory sessionFactory;
+
+    public void setup() {
+        final StandardServiceRegistry registry = new StandardServiceRegistryBuilder()
+                .configure() // configures settings from hibernate.cfg.xml
+                .build();
+        try {
+            sessionFactory = new MetadataSources(registry).buildMetadata().buildSessionFactory();
+        } catch (Exception ex) {
+            StandardServiceRegistryBuilder.destroy(registry);
+        }
+    }
+
+    public void exit() {
+        sessionFactory.close();
+    }
+
+    public void create(SupplierEntity supplier) {
+        Session session = sessionFactory.openSession();
+        session.beginTransaction();
+
+        session.save(supplier);
+
+        session.getTransaction().commit();
+        session.close();
+
+    }
+}
